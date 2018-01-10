d4057bf0580d8a6bbd702c0bb2eec3ee22163388
niloofarmotamed <niloofar.motamed@gmail.com>
2018-01-10 14:52:16 +0200

    refactor Buying component to be a stateless functional component

--- src/components/Buying.js
+++ src/components/Buying.js
@@ -1,28 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 import "../styles/buying.css";
 
-class Buying extends Component {
-  
-  
-  render() {    
-    return (
-      <div className="container">
-        <ul className="buy">
-          {this.props.items && this.props.items.map((item, i) => {
+function Buying(props) {
+  return (
+    <div className="container">
+      <ul className="buy">
+        {props.items &&
+          props.items.map((item, i) => {
             return (
-                <li key={i} className="buying">
-                  <input type="checkbox" checked={this.props.checked} id={item.id} name={item.id} className="input-check"  onChange = {()=>this.props.onChecked && this.props.onChecked(item)}/>
-                  <label htmlFor={item.id} className={this.props.strike}>{item.name}</label>
-                  <span id="number" className={this.props.strike}>{item.quantity} </span>
-                  <button className="delete" onClick = {()=>this.props.onRemove(item)}>X</button>
-                </li>   
+              <li key={i} className="buying">
+                <input
+                  type="checkbox"
+                  checked={props.checked}
+                  id={item.id}
+                  name={item.id}
+                  className="input-check"
+                  onChange={() =>
+                    props.onChecked && props.onChecked(item)
+                  }
+                />
+                <label htmlFor={item.id} className={props.strike}>
+                  {item.name}
+                </label>
+                <span id="number" className={props.strike}>
+                  {item.quantity}{" "}
+                </span>
+                <button
+                  className="delete"
+                  onClick={() => props.onRemove(item)}
+                >
+                  X
+                </button>
+              </li>
             );
           })}
-        </ul>
-      </div>
-     
-    );
-  }
+      </ul>
+    </div>
+  );
 }
+
 
 export default Buying;
